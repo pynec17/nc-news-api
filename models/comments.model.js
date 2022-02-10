52ecb3d52ba8ea0ebcfd886bbd9c6013ad1f4eb5
Clayton <clayton.pyne7@gmail.com>
2022-02-10 23:55:54 +0000

    Reversed article coments order of appearance

--- models/comments.model.js
+++ models/comments.model.js
@@ -2,7 +2,10 @@ const db = require("../db/connection");
 
 exports.selectCommentsByArticleId = (article_id) => {
   return db
-    .query("SELECT * FROM comments WHERE article_id=$1;", [article_id])
+    .query(
+      "SELECT * FROM comments WHERE article_id=$1 ORDER BY created_at DESC;",
+      [article_id]
+    )
     .then(({ rows }) => {
       if (!rows[0]) {
         return Promise.reject({ status: 404, message: "Not Found" });
