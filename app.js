a00d4bcacb4c904f76404953a0fd8c7b64f4a53a
Clayton <clayton.pyne7@gmail.com>
2022-03-08 23:15:49 +0000

    add routers

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { getTopics } = require("./controllers/topics.controller.js");
-const {
-  getArticleByID,
-  patchArticleVotes,
-  getArticles,
-} = require("./controllers/articles.controller");
+
 const {
   getCommentsByArticleID,
   postComment,
@@ -18,29 +13,16 @@ const {
   handlePsqlErrors,
   handleServerErrors,
 } = require("./errors.js");
-const endpoints = require("./endpoints.json");
-const { application } = require("express");
+
 const { getAllUsers, getUser } = require("./controllers/users.controller.js");
+const apiRouter = require("./routers/api.router");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res, next) => {
-  res.send(endpoints);
-});
-
-app.get("/api/topics", getTopics);
-app.get("/api/articles/:article_id", getArticleByID);
-app.patch("/api/articles/:article_id", patchArticleVotes);
-app.get("/api/articles", getArticles);
-app.get("/api/articles/:article_id/comments", getCommentsByArticleID);
-app.post("/api/articles/:article_id/comments", postComment);
-app.delete("/api/comments/:comment_id", removeComment);
-app.get("/api/users", getAllUsers);
-app.get("/api/users/:username", getUser);
-app.patch("/api/comments/:comment_id", patchCommentVotes);
+app.use("/api", apiRouter);
 
 // errors
 app.all("*", handle404s);
